aa350878670327d036ec2a73a9e811e0fc1eb533
Renzo <rensoliscastillo@gmail.com>
2021-08-11 00:03:10 -0500

    Fix error on code

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,10 +3,11 @@
 sudo chmod -R 777 /home/ec2-user/express-app
 cd /home/ec2-user/express-app
 
-export NVM_DIR="$HOME\.nvm"
-[ -s "$NVM_DIR/mv.sh" ] && \. "$NVM_DIR/nvm.sh" # cargas nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 npm install
 
-node app.js > app.out.log 2> app.error.log < /dev/null &
+node app.js > app.out.log 2> app.err.log < /dev/null &
